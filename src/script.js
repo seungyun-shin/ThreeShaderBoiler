49be740aff2f7bf8e48f7f0fd8d1e45da8cf2736
ssy <seungyun-shin@naver.com>
2021-06-09 16:46:56 +0900

    first commit

--- src/script.js
+++ src/script.js
@@ -1,105 +1,48 @@
-import './style.css'
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as dat from 'dat.gui'
+import * as THREE from 'three';
+import fragment from '../shaders/fragment.glsl'
+import vertex from '../shaders/vertex.glsl'
 
-// Debug
-const gui = new dat.GUI()
 
-// Canvas
-const canvas = document.querySelector('canvas.webgl')
+export default class Sketch{
+    constructor(){
 
-// Scene
-const scene = new THREE.Scene()
+        this.renderer = new THREE.WebGLRenderer( { antialias: true } );
+        this.renderer.setSize( window.innerWidth, window.innerHeight );
+        document.getElementById('container').appendChild( this.renderer.domElement );
 
-// Objects
-const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
+        this.camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
+        this.camera.position.z = 1;
+        this.scene = new THREE.Scene();
 
-// Materials
+        this.addMesh();
+        this.time = 0;
+        this.render();
+    }
 
-const material = new THREE.MeshBasicMaterial()
-material.color = new THREE.Color(0xff0000)
+    addMesh(){
+        this.geometry = new THREE.PlaneBufferGeometry(1, 1, 10, 10 );
+	    this.material = new THREE.MeshNormalMaterial({side: THREE.DoubleSide});
+        this.material = new THREE.ShaderMaterial({
+            fragmentShader:fragment,
+            vertexShader:vertex,
+            uniform:{
+                progress: {type:"f", value: 0}
+            },
+            side: THREE.DoubleSide
+        })
+        this.mesh = new THREE.Points( this.geometry, this.material );
+	    this.scene.add( this.mesh );
+    }
 
-// Mesh
-const sphere = new THREE.Mesh(geometry,material)
-scene.add(sphere)
-
-// Lights
-
-const pointLight = new THREE.PointLight(0xffffff, 0.1)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
-scene.add(pointLight)
-
-/**
- * Sizes
- */
-const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+    render(){
+        this.time++;
+        this.mesh.rotation.x += 0.01;
+        this.mesh.rotation.y += 0.02;
+        // console.log(this.time);
+        this.renderer.render( this.scene, this.camera);
+        window.requestAnimationFrame(this.render.bind(this));
+    }
 }
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+new Sketch();
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
-
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
-
-/**
- * Camera
- */
-// Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 0
-camera.position.y = 0
-camera.position.z = 2
-scene.add(camera)
-
-// Controls
-// const controls = new OrbitControls(camera, canvas)
-// controls.enableDamping = true
-
-/**
- * Renderer
- */
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-
-/**
- * Animate
- */
-
-const clock = new THREE.Clock()
-
-const tick = () =>
-{
-
-    const elapsedTime = clock.getElapsedTime()
-
-    // Update objects
-    sphere.rotation.y = .5 * elapsedTime
-
-    // Update Orbital Controls
-    // controls.update()
-
-    // Render
-    renderer.render(scene, camera)
-
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-}
-
-tick()
